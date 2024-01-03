281e8f6c7a2b32418f09346e941d877a2e5b5f45
Tommaso Feltrin <t.feltrin@clivet.it>
2024-01-03 23:22:57 +0100

    bottom navigation bar added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/category_meals_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
+import 'package:meals/screens/tabs_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.pink,
+        primaryColor: Colors.pink,
         colorScheme: const ColorScheme.light(
           onPrimaryContainer: Colors.amber,
           background:Color.fromRGBO(255, 254, 229, 1)
@@ -27,7 +28,13 @@ class MyApp extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
           backgroundColor: Colors.pink,
-          foregroundColor: Colors.white
+          foregroundColor: Colors.white,
+        ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          type: BottomNavigationBarType.shifting,
+          selectedItemColor: Colors.amber,
+          backgroundColor: Colors.pink,
+          unselectedItemColor: Colors.white
         ),
         textTheme: ThemeData.light().textTheme.copyWith(
           bodyLarge: const TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
@@ -42,7 +49,7 @@ class MyApp extends StatelessWidget {
       // home: const CategoriesScreen(),
       initialRoute: '/', //default is '/'
       routes: {
-        '/': (context) => const CategoriesScreen(),
+        '/': (context) => const TabsScreen(),
         CategoryMealsScreen.routeName: (context) => const CategoryMealsScreen(),
         MealDetailScreen.routeName: (context) => const MealDetailScreen(),
       },
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, Object>> _pages = const [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': Favorites(), 'title': 'Your Favorites'}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${_pages[_selectedPageIndex]['title']}'),
+      ),
+      body: _pages[_selectedPageIndex]['page'] as Widget,
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.category),
+            label: 'Categories',
+            backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor
+          ),
+          BottomNavigationBarItem(
+            icon: const Icon(Icons.star),
+            label: 'Favorites',
+            backgroundColor: Theme.of(context).bottomNavigationBarTheme.backgroundColor
+          ),
+        ]
+      ),
+    );
+  }
+}
